f7a3bac2e7a16572b13097f6f77bd2667cdcae33
marioandreseche <110318519+marioandreseche@users.noreply.github.com>
2022-11-24 21:07:02 -0300

    [Mario Echeverria] barra de busqueda

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from "react";
+import "./Styles.scss";
 
- const Header = () => {
+const Header = () => {
   return (
-    <div>header</div>
-  )
-}
+    <header>
+      <div className="container">
+        <div className="div-img">
+          <img src={require("../assets/icons/meli-icono.jpg")} alt="Mercado Libre" />
+        </div>
+        <div className="div-input">
+          <form>
+            <input placeholder="Nunca dejes de buscar"></input>
+          </form>
+        </div>
+        <div>
+          <button value={"buscar"}> Buscar</button>
+        </div>
+      </div>
+    </header>
+  );
+};
 export default Header;
